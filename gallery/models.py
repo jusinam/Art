c96bb1eb53d92a9785e80de0e55755a1f0146a07
DjCooGie <evansonchagwa01@gmail.com>
2020-02-02 20:41:37 +0300

    create Image models class ,3-fields and save/delete functions

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField()
+    image_path = models.ImageField(upload_to = 'gallery/')
+    
+
+    def __str__(self):
+        return self.image_name
+
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def retrieve_all(cls):
+        all_objects = Image.objects.all()
+        for item in all_objects:
+            return item;
