e59d7ac9680b98e5553fd8c60e93205e98c84f20
DjCooGie <evansonchagwa01@gmail.com>
2020-02-02 20:51:50 +0300

    create test class that tests the Image model class

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Image
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.new_image = Image(id=1,image_name='Evans', image_description='Evans is a code enthusiast',image_path='media/gallery/evans.jpg')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+
+     # Testing Save Method
+    def test_save_method(self):
+        self.new_image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    # def tearDown(self):
+    #     Image.objects.all().delete()
