5c3079f4e16c3014768fa0cb4ea16ef85719affa
DjCooGie <evansonchagwa01@gmail.com>
2020-02-03 03:21:11 +0300

    add rout urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,6 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.art,name='art'),
+    url(r'^categories/(\w+)', views.get_category,name='get_category'),
+    url(r'^location/(\w+)', views.get_location,name='get_location'),
+    url(r'^search/',views.search_results, name='search_results'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
