6ea528ff2ddcbb93352d6c254b5032fdbb27c395
DjCooGie <evansonchagwa01@gmail.com>
2020-02-03 03:19:56 +0300

    create Category and Location model classes

--- gallery/models.py
+++ gallery/models.py
@@ -1,16 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=40, unique=True)
+    
+    
+    def __str__(self):
+        return self.category_name
+
+
+    def save_category(self):
+        self.save()
+        
+class Location(models.Model):
+    location_name = models.CharField(max_length=30, unique=True)
+    
+    
+    def __str__(self):
+            return self.location_name
+
+
+    def save_location(self):
+        self.save()
+
+
+
 
 class Image(models.Model):
     image_name = models.CharField(max_length =30)
     image_description = models.TextField()
     image_path = models.ImageField(upload_to = 'gallery/')
-    
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
+
+    
     def __str__(self):
         return self.image_name
-
 
     def save_image(self):
         self.save()
@@ -18,7 +44,29 @@ class Image(models.Model):
         self.delete()
 
     @classmethod
+    def search_by_category(cls,search_term):
+        search_result = cls.objects.filter(image_category__category_name__icontains=search_term)
+        return search_result
+
+    @classmethod
     def retrieve_all(cls):
         all_objects = Image.objects.all()
         for item in all_objects:
             return item;
+
+    @classmethod
+    def get_image_by_id(cls,incoming_id):
+        image_result = cls.objects.get(id=incoming_id)
+        return image_result
+
+    @classmethod
+    def update_image(cls,current_value,new_value):
+        obtained_object = Image.objects.filter(image_name=current_value).update(image_name=new_value)
+        return obtained_object
+    
+    @classmethod
+    def obtain_by_location(cls,location):
+        obtained_location = cls.objects.filter(image_location__location_name__icontains=location)
+        return obtained_location
+
+
